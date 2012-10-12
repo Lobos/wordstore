f4cedb824cc155dbfbe3dfeeca506a737b0566ae
lobos <lobos841@gmail.com>
2012-10-12 11:03:44 +0800

    data位置

--- web/import_dict.py
+++ web/import_dict.py
@@ -9,22 +9,31 @@ def import_dict():
     db.Dictionary.generate_index(db.dictionary)
 
     index = 0
-    f = open('data/dict.txt', 'r')
+    f = open('../data/dict.txt', 'r')
     for line in f:
         w, _, d = line.partition('\t')
         word = db.Dictionary()
         word['word'] = unicode(w, errors='ignore')
         word['def'] = unicode(d, errors='ignore')
         word['inflections'] = [unicode(w, errors='ignore')]
+        word['sound'] = get_sound(word['word'])
         word.save()
         print w
         index += 1
 
     print '%s done.' % index
 
+def get_sound(word):
+    if not word:
+        return u''
+    if not word[0].isalpha():
+        return u''
+
+    return u'/sound/%s/%s.ogg' % (word[0].upper(), word)
+
 def import_inflections():
     db = get_db()
-    f = open('data/inflections.csv')
+    f = open('../data/inflections.csv')
     for line in f:
         infs = [unicode(s, errors='ignore') for s in line.split(',') if len(s) > 1]
         if len(infs) > 1:
@@ -36,7 +45,7 @@ def import_inflections():
 
 def import_phon():
     db = get_db()
-    f = open('data/phon.txt')
+    f = open('../data/phon.txt')
     for line in f:
         w, p = line.split('\t')
         print w, p
